1e179a35ae9e467308263e6ebd6761ee21ebf073
johnbian <byh19920310@126.com>
2023-02-06 15:18:23 +0800

    Delete index.js
    feat: 删除无效文件

--- index.js
+++
@@ -1,6 +0,0 @@
-
-$('[data-toggle="popover"]').popover({
-  content: `<div class="center">微信扫一扫</div>
-            <img class="center wx-img" src="https://img2.baidu.com/it/u=648859196,2887003672&fm=253&fmt=auto?w=180&h=180"/>`,
-  html: true
-});
